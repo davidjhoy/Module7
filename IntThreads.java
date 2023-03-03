ad00567ce9244eea7d7de407911646aeeebf1d3f
david <davidjhoyjr@gmail.com>
2023-03-02 21:36:12 -0500

    last

--- IntThreads.java
+++ IntThreads.java
@@ -2,32 +2,49 @@ public class IntThreads implements Runnable{
     Thread thrd;
     Integers inObj;
 
-    public IntThreads(String name){
-        thrd = new Thread(name);
-    }
-
-
-    public void startAndRun(String name, Integers intsObj){
-        IntThreads th = new IntThreads(name);
-        th.thrd.start();
-
+    public IntThreads(String name,  Integers intsObj){
+        thrd = new Thread(this, name);
         inObj = intsObj;
     }
 
 
+    public static IntThreads startAndRun(String name, Integers insObj){
+        IntThreads th = new IntThreads(name, insObj);
+        th.thrd.start();
+        return th;
+
+        
+    }
+
+
     public void run(){
+       
         if (thrd.getName().compareTo("Pusher") == 0){
+            
             for (int i = 1; i <11; i++){
-                inObj.pushInt();
+                inObj.pushInt(i, true);
             }
+            inObj.pushInt(0, false);
         }else{
-            for(int i = 1; i < 11; i++){
-                inObj.pullint();
+            for(int i = 0; i < 10; i++){
+                inObj.pullint(true);
             }
+            inObj.pullint(false);
         }
     }
 
     public static void main(String[] args){
-        
+        //Create an Integers obj
+        Integers inObj = new Integers();
+
+        IntThreads thr1 = IntThreads.startAndRun("Pusher", inObj);
+        IntThreads thr2 = IntThreads.startAndRun("Puller", inObj);
+
+        try{
+            thr1.thrd.join();
+            thr2.thrd.join();
+        }catch(InterruptedException ex){
+            System.out.println("System interrupted: " + ex);
+        }
     }
 }
--- Integers.java
+++ Integers.java
@@ -1,16 +1,59 @@
 public class Integers {
     private int V;
+    private boolean isEmpty;
 
-    public Integers(int i){
-        V = i;
+    public Integers(){
+        //int store is initially false
+        isEmpty = true;
+        
     }
     
 
-    public synchronized void pushInt(){
+    public synchronized void pushInt(int i, boolean running){
+
+        if (!running){
+            isEmpty = false;
+            notify();
+            return;
+        }
+
+        V = i;
+        isEmpty = false;
+        notify();
+
+        try{
+            while(!isEmpty){
+                wait();
+            }
+
+        }catch(InterruptedException ex){
+            System.out.println("System Interrupted");
+        }
 
     }
+    
 
-    public synchronized void pullint(){
+    public synchronized void pullint(boolean running){
+
+        if (!running){
+            isEmpty = true;
+            notify();
+            return;
+        }
+
+        System.out.println("Value of int store is: " + V);
+        isEmpty = true;
+        notify();
+
+
+        try{
+            while(isEmpty){
+                wait();
+            }
+
+        }catch(InterruptedException ex){
+            System.out.println("System Interrupted");
+        }
 
     }
 }
